265838e27d3fdb4650f3686ed47166c7d36b2f27
Douglas Rodrigues Pereira <douglasrodrigues20161@outlook.com>
2021-03-06 19:34:23 -0300

    criando widget(input).

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:aog/widgets/input.widget.dart';
 import 'package:aog/widgets/loading-button.widget.dart';
 import 'package:aog/widgets/logo.widget.dart';
+import 'package:aog/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
@@ -34,37 +35,9 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
-          Container(
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  "Compensa utilizar álcool",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 40,
-                    fontFamily: "Big Shoulder Display",
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                LoadingButton(
-                  busy: false,
-                  invert: true,
-                  func: () {},
-                  text: "CALCULAR NOVAMENTE",
-                ),
-              ],
-            ),
+          Success(
+            result: "Compensa utilizar X",
+            reset: () {},
           ),
           Input(
             controller: _gasolinaController,
